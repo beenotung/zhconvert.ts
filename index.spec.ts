01d30bd889304f0c0aee9c7ecf6c29f8d0bd0c8d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-04-17 19:33:12 +0800

    test: update test cases to use the same sentence

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import {
+  Converter,
   translate,
   translateIntoSimplified,
   translateIntoTraditional,
@@ -23,59 +24,81 @@ describe('translateIntoSimplified', () => {
 })
 
 describe('translate to all variants', () => {
-  let text = '記憶體不足！沒辦法在視訊聊天時播放影片。'
+  let text = zh_cn
+
+  async function test(converter: Converter) {
+    let result = await translate({ text, converter })
+    expect(result.data.converter).to.equal(converter)
+    return result.data.text
+  }
 
   it('should translate into Simplified variant (簡體化)', async () => {
-    let result = await translate({ text, converter: 'Simplified' })
-    expect(result).to.equal('记忆体不足！没办法在视讯聊天时播放影片。')
+    let result = await test('Simplified')
+    expect(result).to.equal(zh_cn)
   })
 
   it('should translate into Traditional variant (繁體化)', async () => {
-    let result = await translate({ text, converter: 'Traditional' })
-    expect(result).to.equal('記憶體不足！沒辦法在視訊聊天時播放影片。')
+    let result = await test('Traditional')
+    expect(result).to.equal(
+      '幹，快乾杯，芒果乾好吃干他什麼事，別干擾我吃芒果乾',
+    )
   })
 
   it('should translate into China variant (中國化)', async () => {
-    let result = await translate({ text, converter: 'China' })
-    expect(result).to.equal('记忆体不足！没办法在视频聊天时播放视频。')
+    let result = await test('China')
+    expect(result).to.equal(
+      '干，快干杯，芒果干好吃干他什么事，别干扰我吃芒果干',
+    )
   })
 
   it('should translate into Hongkong variant (香港化)', async () => {
-    let result = await translate({ text, converter: 'Hongkong' })
-    expect(result).to.equal('記憶體不足！沒辦法在視訊聊天時播放影片。')
+    let result = await test('Hongkong')
+    expect(result).to.equal(
+      '幹，快乾杯，芒果乾好吃干他甚麼事，別干擾我吃芒果乾',
+    )
   })
 
   it('should translate into Taiwan variant (台灣化)', async () => {
-    let result = await translate({ text, converter: 'Taiwan' })
-    expect(result).to.equal('記憶體不足！沒辦法在視訊聊天時播放影片。')
+    let result = await test('Taiwan')
+    expect(result).to.equal(
+      '幹，快乾杯，芒果乾好吃干他什麼事，別干擾我吃芒果乾',
+    )
   })
 
   it('should translate into Pinyin variant (拼音化)', async () => {
-    let result = await translate({ text, converter: 'Pinyin' })
+    let result = await test('Pinyin')
     expect(result).to.equal(
-      'ji4 yi4 ti3 bu4 zu2！mei2 ban4 fa3 zai4 shi4 xun4 liao2 tian1 shi2 bo1 fang4 ying3 pian1。',
+      'gan1，kuai4 gan1 bei1，mang2 guo3 gan4 hao4 chi1 gan1 ta1 shen2 me5 shi4，bie2 gan1 rao3 wo3 chi1 mang2 guo3 gan4',
     )
   })
 
   it('should translate into Bopomofo variant (注音化)', async () => {
-    let result = await translate({ text, converter: 'Bopomofo' })
+    let result = await test('Bopomofo')
     expect(result).to.equal(
-      'ㄐㄧˋ ㄧˋ ㄊㄧˇ ㄅㄨˋ ㄗㄨˊ ！ ㄇㄟˊ ㄅㄢˋ ㄈㄚˇ ㄗㄞˋ ㄕˋ ㄒㄩㄣˋ ㄌㄧㄠˊ ㄊㄧㄢ ㄕˊ ㄅㄛˋ ㄈㄤˋ ㄧㄥˇ ㄆㄧㄢˋ 。',
+      'ㄍㄢˋ ， ㄎㄨㄞˋ ㄍㄢ ㄅㄟ ， ㄇㄤˊ ㄍㄨㄛˇ ㄍㄢ ㄏㄠˋ ㄔ ㄍㄢ ㄊㄚ ㄕㄣˊ ㄇㄜ˙ ㄕˋ ， ㄅㄧㄝˊ ㄍㄢ ㄖㄠˇ ㄨㄛˇ ㄔ ㄇㄤˊ ㄍㄨㄛˇ ㄍㄢ',
     )
   })
 
   it('should translate into Mars variant (火星化)', async () => {
-    let result = await translate({ text, converter: 'Mars' })
-    expect(result).to.equal('記憶體ㄅ足！迷辦法在視訊聊天時播放影片。')
+    let result = await test('Mars')
+    let variants = [
+      '幹，快乾杯，芒果乾毫ㄘ干ㄊ什ㄇ事，別干擾偶ㄘ芒果乾',
+      '幹，快乾杯，芒果乾毫ㄘ干ㄊ什ㄇ事，別干擾窩ㄘ芒果乾',
+    ]
+    expect(variants).to.include(result)
   })
 
   it('should translate into WikiSimplified variant (維基簡體化)', async () => {
-    let result = await translate({ text, converter: 'WikiSimplified' })
-    expect(result).to.equal('记忆体不足！没办法在视讯聊天时播放影片。')
+    let result = await test('WikiSimplified')
+    expect(result).to.equal(
+      '干，快干杯，芒果干好吃干他什么事，别干扰我吃芒果干',
+    )
   })
 
   it('should translate into WikiTraditional variant (維基繁體化)', async () => {
-    let result = await translate({ text, converter: 'WikiTraditional' })
-    expect(result).to.equal('記憶體不足！沒辦法在視訊聊天時播放影片。')
+    let result = await test('WikiTraditional')
+    expect(result).to.equal(
+      '干，快乾杯，芒果乾好吃干他什麼事，別干擾我吃芒果乾',
+    )
   })
 })
